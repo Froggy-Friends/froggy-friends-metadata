b1773c7c7b41779d66e1758304e163bcf236517e
Alfonso <alfonso@viruett.com>
2023-02-02 20:52:47 -0800

    adds item contract endpoint

--- src/app.controller.ts
+++ src/app.controller.ts
@@ -1,6 +1,7 @@
 import { Controller, Get, Param } from '@nestjs/common';
 import { AppService } from './app.service';
 import { ItemMetadata } from './item/item.metadata';
+import { ItemsContractMetadata } from './models/ItemsContractMetadata';
 import { Metadata } from './models/Metadata';
 
 @Controller()
@@ -10,6 +11,18 @@ export class AppController {
   @Get('/:id')
   getFrog(@Param('id') frogId: number): Promise<Metadata> {
     return this.appService.getFrog(frogId);
+  }
+
+  @Get('/item/contract')
+  getItemsContractMetadata(): ItemsContractMetadata {
+    return {
+      "name": "Froggy Shop",
+      "description": "The official Froggy Friends - Ribbit Market items",
+      "image": "https://froggyfriends.mypinata.cloud/ipfs/QmWTpDVCUewNUS3Rix4rCV1GmMUDF9E8EbAtBZMDztjohh",
+      "external_link": "https://froggyfriendsnft.com",
+      "seller_fee_basis_points": 1000,
+      "fee_recipient": "0x6b01aD68aB6F53128B7A6Fe7E199B31179A4629a"
+    }
   }
 
   @Get('/item/:id')
---
+++ src/models/ItemsContractMetadata.ts
@@ -0,0 +1,8 @@
+export interface ItemsContractMetadata {
+    name: string,
+    description: string,
+    image: string,
+    external_link: string,
+    seller_fee_basis_points: number,
+    fee_recipient: string
+}
