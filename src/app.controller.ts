80690c058277df74a3e63daaa7876a06d00d13c1
alfonsoaranzazu <alfonso@viruett.com>
2023-02-02 18:52:57 -0800

    adds item endpoint

--- src/app.controller.ts
+++ src/app.controller.ts
@@ -1,5 +1,7 @@
 import { Controller, Get, Param } from '@nestjs/common';
 import { AppService } from './app.service';
+import { Item } from './item/item.entity';
+import { ItemMetadata } from './item/item.metadata';
 import { Metadata } from './models/Metadata';
 
 @Controller()
@@ -7,7 +9,12 @@ export class AppController {
   constructor(private readonly appService: AppService) {}
 
   @Get('/:id')
-  getItem(@Param('id') frogId: number): Promise<Metadata> {
+  getFrog(@Param('id') frogId: number): Promise<Metadata> {
     return this.appService.getFrog(frogId);
+  }
+
+  @Get('/item/:id')
+  getItem(@Param('id') itemId: number): Promise<ItemMetadata> {
+    return this.appService.getItem(itemId);
   }
 }
