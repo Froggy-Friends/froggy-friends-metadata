2b58bf871822c83c416f134951eb0751318f2d37
Alfonso <alfonso@viruett.com>
2023-07-02 15:27:53 -0700

    enable image metadata

--- src/models/Metadata.ts
+++ src/models/Metadata.ts
@@ -3,9 +3,9 @@ import { Attribute } from "./Attribute";
 export interface Metadata {
   name: string;
   description: string;
-  // image: string;
-  // image3d: string;
-  // imagePixel: string;
+  image: string;
+  image3d: string;
+  imagePixel: string;
   animation_url: string;
   edition: number;
   date: string;
