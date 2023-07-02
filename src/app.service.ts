2749e6020e59739ec2969a86bb28d2eddbe99f2b
Alfonso <alfonso@viruett.com>
2023-07-02 01:29:19 -0700

    disable image links

--- src/app.service.ts
+++ src/app.service.ts
@@ -31,9 +31,9 @@ export class AppService {
     let metadata: Metadata = {
       name: frog.name,
       description: frog.description,
-      image: `${this.froggyGatewayUrl}/${frog.cid2d}`,
-      image3d: `${this.froggyGatewayUrl}/${frog.cid3d}`,
-      imagePixel: `${this.froggyGatewayUrl}/${frog.cidPixel}`,
+      // image: `${this.froggyGatewayUrl}/${frog.cid2d}`,
+      // image3d: `${this.froggyGatewayUrl}/${frog.cid3d}`,
+      // imagePixel: `${this.froggyGatewayUrl}/${frog.cidPixel}`,
       animation_url: `${this.animationUrl}/${frog.cid2d}/${frog.cid3d}/${frog.cidPixel}`,
       edition: frog.edition,
       date: frog.date,
