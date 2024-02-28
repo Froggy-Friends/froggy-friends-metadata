79a7b984b35726a0c62aaa56526206665589badb
0xFonzy <fonzy@fonzy.io>
2024-02-28 11:07:49 -0800

    updates item name

--- src/app.controller.ts
+++ src/app.controller.ts
@@ -45,7 +45,7 @@ export class AppController {
   @Get('/tadpole/:id')
   getTadpole(): TadpoleMetadata {
     return {
-      name: 'Tadpole Evolution',
+      name: 'Tadpoles',
       description: 'The official ERC404 Collection of Froggy Friends',
       image: 'https://froggyfriends.mypinata.cloud/ipfs/QmWqdzCvPYyifzoPYiwehm1gkGLiYVFzWDYz9o3n7nLHMG',
       attributes: [],
