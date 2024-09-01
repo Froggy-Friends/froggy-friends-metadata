1ab7288794c3c43eaffcedfa1f0e28fe0bc8a24f
0xFonzy <fonzy@fonzy.io>
2024-08-31 18:01:07 -0700

    updates tadpole endpoint

--- src/tadpole/tadpole.controller.ts
+++ src/tadpole/tadpole.controller.ts
@@ -1,6 +1,7 @@
 import { Controller, Get, Param } from '@nestjs/common';
 import { TadpoleMetadata } from '../models/TadpoleMetadata';
 import { TadpoleService } from './tadpole.service';
+import BigNumber from 'bignumber.js';
 
 @Controller('tadpole')
 export class TadpoleController {
@@ -8,7 +9,13 @@ export class TadpoleController {
 
   @Get('/:id')
   async getTadpole(@Param('id') id: string): Promise<TadpoleMetadata> {
-    const tadpole = await this.tadpoleService.findTadpole(+id);
+    const prefix = new BigNumber(
+      '57896044618658097711785492504343953926634992332820282019728792003956564819968',
+    );
+    const encodedId = new BigNumber(id);
+    const decodedId = encodedId.minus(prefix);
+    const decodedTokenId = decodedId.toNumber();
+    const tadpole = await this.tadpoleService.findTadpole(decodedTokenId);
     return this.tadpoleService.tadpoleToMetadata(tadpole);
   }
 }
